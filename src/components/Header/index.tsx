92138712cd676646cd3affb8737d28f6313b151b
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-07-16 20:09:30 -0300

    feat: component SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { ActiveLink } from '../ActiveLink';
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -15,6 +16,7 @@ export function Header() {
             <a>Posts</a>
           </ActiveLink>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
